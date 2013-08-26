e7eac8c6262f3b902449781630e18b1bb0302222
Brian Yee <byee322@gmail.com>
2013-08-26 14:04:57 -0700

    Inital Commit
    All html, css, js libraries, and images

---
+++ js/animations.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+		// $(window).scroll(function() {
+		// 	$('#').each(function(){
+		// 	var imagePos = $(this).offset().top;
+
+		// 	var topOfWindow = $(window).scrollTop();
+		// 		if (imagePos < topOfWindow+400) {
+		// 			$(this).addClass("slideUp");
+		// 		}
+		// 	});
+		// });
+});
