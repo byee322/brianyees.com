e18098495604ce0722608b53a1fe0845329b0425
Brian Yee <byee322@gmail.com>
2014-07-15 13:40:03 -0700

    optimization of min js files and images

---
+++ js copy/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	// $("img#preview").mouseover(function(){
+	// 	$(this).css("width","35%");
+	// 	$(this).css("height", "35%");
+	// }).mouseout(function(){
+	// 	$(this).css("width","25%");
+	// 	$(this).css("height", "25%");
+	// });
+});
